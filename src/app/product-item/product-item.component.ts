3d1a0f2b41a96e32c05bcbd180d51922890c7ab1
MKeulimzhanov <m_keulimzhanov@kbtu.kz>
2022-03-25 13:40:41 +0600

    3 files modified

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products-item';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-item',
@@ -10,7 +11,10 @@ import { Product, products } from '../products-item';
 export class ProductItemComponent implements OnInit {
   product: Product | undefined;
   n = 0;
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
@@ -19,8 +23,17 @@ export class ProductItemComponent implements OnInit {
       (product) => product.id === productIdFromRoute
     );
   }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+
   LIKE() {
     this.n += 1;
+  }
+  ResetLIKE() {
+    this.n = 0;
   }
 }
 /*
